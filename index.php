c220a54385e03ac00f68e5a33795f002e03902ec
BiggyDev <vincent.carpentier111@gmail.com>
2018-11-07 15:50:43 +0100

    one

--- index.php
+++ index.php
@@ -5,24 +5,12 @@
     <title></title>
   </head>
   <body>
-<<<<<<< HEAD
-    <p>byubu</p>
-    <?php
-    $vincent = 'je suis vincent';
-=======
 
-
-    echo $vincent .'et je suis chinoise';
-    ?>
-    <p>on fait quoi</p>
-    >>>>>>> 94e0eeea2b29d53fd41f34ec223b6653af64f6e8
-
-<<<<<<< HEAD
-<p> wsh jeanluc et vincent sa fout quoi le php c'est comment ?</p>
-
-
-
-<p>byuuvyuavyvtobaibay</p>
-
+    <p>Le Renard s'en saisit, et dit : Mon bon Monsieur,
+                   Apprenez que tout flatteur
+          Vit aux dépens de celui qui l'écoute.
+        Cette leçon vaut bien un fromage sans doute.
+                Le Corbeau honteux et confus
+        Jura, mais un peu tard, qu'on ne l'y prendrait plus.</p>
   </body>
 </html>

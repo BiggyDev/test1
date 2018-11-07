1d7352ebc6273e74d1677167a52580831b3fa04d
mimoo76 <totoriina76380@gmail.com>
2018-11-07 15:47:59 +0100

    nouveau

--- index.php
+++ index.php
@@ -5,24 +5,23 @@
     <title></title>
   </head>
   <body>
-<<<<<<< HEAD
-    <p>byubu</p>
-    <?php
-    $vincent = 'je suis vincent';
-=======
-
-
-    echo $vincent .'et je suis chinoise';
-    ?>
-    <p>on fait quoi</p>
-    >>>>>>> 94e0eeea2b29d53fd41f34ec223b6653af64f6e8
-
-<<<<<<< HEAD
-<p> wsh jeanluc et vincent sa fout quoi le php c'est comment ?</p>
+<h1>le corbeau et vincent</h1>
 
 
 
-<p>byuuvyuavyvtobaibay</p>
 
+
+
+
+
+
+
+
+ <p> Sans mentir, si votre ramage
+           Se rapporte à votre plumage,
+     Vous êtes le Phénix des hôtes de ces bois.
+À ces mots le Corbeau ne se sent pas de joie,
+           Et pour montrer sa belle voix,
+   Il ouvre un large bec, laisse tomber sa proie.</p>
   </body>
 </html>
